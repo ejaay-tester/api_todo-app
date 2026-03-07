caa82494d4634289c3cb154d704346e997d95910
ejaay-tester <ejaaay.tester@gmail.com>
2026-03-07 18:12:01 +0800

    Add the login with JWT in the authController.ts

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -9,6 +9,7 @@ import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 import { Request, Response } from "express"
 
+// REGISTER USER
 export const registerUser = async (req: Request, res: Response) => {
   const { email, password } = req.body
 
@@ -20,4 +21,34 @@ export const registerUser = async (req: Request, res: Response) => {
   })
 
   res.json(user)
+}
+
+// LOGIN USER WITH JWT
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    return res.status(401).json({ message: "Invalid login credentials!" })
+  }
+
+  const valid = await bcrypt.compare(password, user.password)
+
+  if (!valid) {
+    return res.status(401).json({ message: "Invalid login credentials!" })
+  }
+
+  // Check if the JWT_SECRET exists
+  if (!process.env.JWT_SECRET) {
+    throw new Error(
+      "FATAL ERROR: JWT_SECRET is not defined in environment variables.",
+    )
+  }
+
+  const userToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  })
+
+  res.json({ userToken })
 }
