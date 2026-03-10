6ba942ca0a2076a7ee73836e2a7d7a617e124f53
ejaay-tester <ejaaay.tester@gmail.com>
2026-03-10 16:37:21 +0800

    Remove the authMiddleware on the /register and /login routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -7,11 +7,11 @@ const router = express.Router()
 /**
  * Register a new user
  */
-router.post("/register", authMiddleware, registerUser) // POST /api/register
+router.post("/register", registerUser) // POST /api/register
 
 /**
  * Login user and return JSON Web Token or JWT
  */
-router.post("/login", authMiddleware, loginUser) // POST /api/login
+router.post("/login", loginUser) // POST /api/login
 
 export default router
