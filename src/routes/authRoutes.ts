7c1b4894347420b870693eb480af58b0e320418f
ejaay-tester <ejaaay.tester@gmail.com>
2026-03-11 01:01:43 +0800

    Integrate the validateRequest and authSchemas in authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,17 +1,18 @@
 import express from "express"
 import { registerUser, loginUser } from "../controllers/authController"
-import { authMiddleware } from "../middleware/authMiddleware"
+import { validateRequest } from "../middleware/validationRequest"
+import { registerSchema, loginSchema } from "../schemas/authSchemas"
 
 const router = express.Router()
 
 /**
  * Register a new user
  */
-router.post("/register", registerUser) // POST /api/register
+router.post("/register", validateRequest(registerSchema), registerUser) // POST /api/register
 
 /**
  * Login user and return JSON Web Token or JWT
  */
-router.post("/login", loginUser) // POST /api/login
+router.post("/login", validateRequest(loginSchema), loginUser) // POST /api/login
 
 export default router
